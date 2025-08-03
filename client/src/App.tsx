8524f142df52ab3950226039f7a6c1c790cc749a
Matt Webber <dev.matthew.lee.webber@gmail.com>
2025-08-02 22:27:24 -0400

    feat: add recharts and chart components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Game from './Game';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
       <h1>retrotrader</h1>
-      <Game />
+      <Home />
     </div>
   );
 }
---
+++ client/src/Data.tsx
@@ -0,0 +1,12 @@
+import ChartLineLabel from './components/chart/chartLineLabel';
+
+const Data = () => {
+  return (
+    <div>
+      <h1>Data Visualization</h1>
+      <ChartLineLabel />
+    </div>
+  );
+};
+
+export default Data;
